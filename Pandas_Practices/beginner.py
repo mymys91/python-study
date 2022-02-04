8a2142a7f9e624ad70d1902540a175d68dd0d49c
My Dinh <thanhmy91@gmail.com>
2022-02-04 13:51:57 +0700

    learn Math & pandas

---
+++ Pandas_Practices/beginner.py
@@ -0,0 +1,7 @@
+import pandas
+
+df1 = pandas.DataFrame([[2,4,6],[10,20,30]],columns=['Price','Age','Value'])
+print(df1)
+
+df2 = pandas.DataFrame([{"Name": "John","Lastname": "Smith"},{"Name": "Jack"}])
+print(df2)
